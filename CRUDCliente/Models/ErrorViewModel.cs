f5263153814af4d5d953c4f61538bf1195c81645
Guilherme Silva <guilherme.dasilvanascimento@gmail.com>
2019-11-06 20:26:50 -0300

    Adicionar arquivos de projeto.

---
+++ CRUDCliente/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDCliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
